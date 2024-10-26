28bbc107fa06688a71ca499d434950f511a2abeb
Laion Corcino <laioncorcino@gmail.com>
2024-10-26 20:33:22 -0300

    [feat] starting project

---
+++ src/test/java/com/corcino/order_food/OrderFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corcino.order_food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
